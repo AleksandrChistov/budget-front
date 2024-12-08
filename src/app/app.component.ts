bf3e6a906336509907f8d586adc657bc25039ba9
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2024-12-08 20:53:13 +0700

    install LTS version of Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {PrimeNG} from 'primeng/config';
+import {definePreset} from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}'
+    }
+  }
+});
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
 })
 export class AppComponent {
-  title = 'budget';
+  public config: PrimeNG = inject(PrimeNG);
+
+  constructor() {
+    this.config.theme.set({ preset: MyPreset });
+  }
 }
