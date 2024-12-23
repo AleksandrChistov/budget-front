1a4acdfad459678d4f8bd72aa8f788aacede2329
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2024-12-23 21:11:57 +0700

    implement all budgets pages

--- src/app/pages/budgets/components/pages/expenses/expenses.component.ts
+++ src/app/pages/budgets/components/pages/expenses/expenses.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { take } from 'rxjs';
+import { HeaderComponent } from "../../header/header.component";
+import { TableBudgetsComponent } from "../../table-budgets/table-budgets.component";
+import { BudgetService } from '../../../services/budget.service';
+import { LabelsService } from '../../../../../shared/services/labels.service';
+import { Option } from '../../../../../shared/interfaces/option.interface';
+import { Budget } from '../../../interfaces/budget.interface';
+import { BudgetTypes } from '../../../enums/budget.enum';
 
 @Component({
   selector: 'app-budgets-expenses',
   standalone: true,
-  imports: [],
+    imports: [
+        HeaderComponent,
+        TableBudgetsComponent
+    ],
   templateUrl: './expenses.component.html',
   styleUrl: './expenses.component.scss'
 })
-export class ExpensesComponent {
+export class ExpensesComponent implements OnInit {
+  budgetService = inject(BudgetService);
+  labelsService = inject(LabelsService);
+  destroyRef = inject(DestroyRef);
 
+  departmentLabels = toSignal<Option<number>[], []>(this.labelsService.getDepartments(), { initialValue: [] });
+  budgetLabels = signal<Option<number>[]>([]);
+  budget = signal<Budget>({} as Budget);
+
+  ngOnInit(): void {
+    this.getBudgetLabels();
+  }
+
+  budgetChanged(id: number): void {
+    console.log('budgetChanged ', id);
+    if (id) {
+      this.budgetService.get(id).pipe(
+        take(1),
+        takeUntilDestroyed(this.destroyRef)
+      ).subscribe((budget: Budget) => this.budget.set(budget));
+    }
+  }
+
+  departmentChanged(id: number): void {
+    this.getBudgetLabels(id);
+  }
+
+  saveBudget(budget: Budget): void {
+    console.log('saveBudget', budget);
+    // TODO: make a request to save a new budget
+  }
+
+  deleteBudget(id: number): void {
+    console.log('deleteBudget', id);
+    // TODO: make a request to delete a budget
+  }
+
+  saveToExcel(): void {
+    console.log('saveToExcel', this.budget());
+    // TODO: make a request to save a budget to excel and download this file
+  }
+
+  getFromExcel(): void {
+    // TODO: open a modal to upload excel file
+  }
+
+  private getBudgetLabels(departmentId?: number): void {
+    console.log('departmentId ', departmentId);
+    this.labelsService.getBudgets(BudgetTypes.EXPENSES, departmentId)
+      .pipe(
+        take(1),
+        takeUntilDestroyed(this.destroyRef)
+      ).subscribe(budgetLabels => this.budgetLabels.set(budgetLabels));
+  }
 }
